8cec25deb42a2946ece2990a40a97f756f82bd3c
Nathan Galindo <nathan.zebedee@gmail.com>
2022-07-13 16:56:39 -0500

    added contact us popup

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 import backgroundImage from '@/images/background-newsletter.jpg'
 
-import { PopupButton } from '@typeform/embed-react'
+import { Sidetab, Widget, PopupButton } from '@typeform/embed-react'
 
 // https://discord.gg/EzBpDrNYa7 DISCORD
 // https://cfy2dkiqhp9.typeform.com/to/K79B5cdo JOIN
@@ -27,37 +27,44 @@ function ArrowRightIcon(props) {
 
 export function Newsletter() {
   return (
-    <section id="newsletter" aria-label="Newsletter">
-      <Container>
-        <div className="relative -mx-4 overflow-hidden bg-indigo-50 py-20 px-4 sm:-mx-6 sm:px-6 md:mx-0 md:rounded-5xl md:px-16 xl:px-24 xl:py-36">
-          <Image
-            className="absolute left-1/2 top-0 translate-x-[-10%] translate-y-[-45%] lg:translate-x-[-32%]"
-            src={backgroundImage}
-            alt=""
-            width={919}
-            height={1351}
-            unoptimized
-          />
-          <div className="relative mx-auto grid max-w-2xl grid-cols-1 gap-x-32 gap-y-14 xl:max-w-none xl:grid-cols-2">
-            <div>
-              <p className="font-display text-4xl font-medium tracking-tighter text-pink-900 sm:text-5xl">
-                Connect with us!
-              </p>
-              <p className="mt-4 text-lg tracking-tight text-pink-900">
-                If you are interested in collaborating with us, or have any questions, please
-                use the link below to get in contact with us.
-              </p>
+    <>
+      <section id="newsletter" aria-label="Newsletter">
+        <Container>
+          <div className="relative -mx-4 overflow-hidden bg-indigo-50 py-20 px-4 sm:-mx-6 sm:px-6 md:mx-0 md:rounded-5xl md:px-16 xl:px-24 xl:py-36">
+            <Image
+              className="absolute left-1/2 top-0 translate-x-[-10%] translate-y-[-45%] lg:translate-x-[-32%]"
+              src={backgroundImage}
+              alt=""
+              width={919}
+              height={1351}
+              unoptimized
+            />
+            <div className="relative mx-auto grid max-w-2xl grid-cols-1 gap-x-32 gap-y-14 xl:max-w-none xl:grid-cols-2">
+              <div>
+                <p className="font-display text-4xl font-medium tracking-tighter text-pink-900 sm:text-5xl">
+                  Ready to dive in?
+                </p>
+                <p className="mt-4 text-lg tracking-tight text-pink-900">
+                  If you want to learn more about us and what we do, join or Discord
+                  server and get involved with the organization using the link below!
+                </p>
+              </div>
+              <form>
+                <h3 className="text-lg font-semibold tracking-tight text-pink-600 hover:text-pink-900">
+                  <a href='https://discord.gg/EzBpDrNYa7' target='_blank' rel='noreferrer' id="RbfWKOhX" style={{ fontSize: 20 }} className="my-button">
+                    Get in contact with us <span aria-hidden="true">&#8594;</span>
+                  </a>
+                </h3>
+              </form>
             </div>
-            <form>
-              <h3 className="text-lg font-semibold tracking-tight text-pink-600 hover:text-pink-900">
-                <PopupButton id="RbfWKOhX" style={{ fontSize: 20 }} className="my-button">
-                  Get in contact with us <span aria-hidden="true">&#8594;</span>
-                </PopupButton>
-              </h3>
-            </form>
           </div>
-        </div>
-      </Container>
-    </section>
+        </Container>
+      </section>
+      <div>
+        <Sidetab id="RbfWKOhX" style={{ fontSize: 20 }} className="my-button">
+          Get in contact with us <span aria-hidden="true">&#8594;</span>
+        </Sidetab>
+      </div>
+    </>
   );
 }
