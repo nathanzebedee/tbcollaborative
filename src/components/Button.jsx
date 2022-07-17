8904f719d0aa4f7411a56b5340dd8be4fdeb10d2
Nathan Galindo <nathan.zebedee@gmail.com>
2022-07-17 13:50:44 -0500

    made bg color transparent for buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import clsx from 'clsx'
 
 export function Button({ href, className, ...props }) {
   className = clsx(
-    'transition-all duration-200 inline-flex justify-center rounded-2xl bg-pink-600 py-3 px-4 text-base font-semibold text-white hover:bg-[#ffeefe] hover:text-pink-600 border-2 border-transparent hover:border-pink-600 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-500 active:text-white/70',
+    'transition-all duration-200 inline-flex justify-center rounded-2xl bg-pink-600 py-3 px-4 text-base font-semibold text-white hover:bg-transparent hover:text-pink-600 border-2 border-transparent hover:border-pink-600 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-500 active:text-white/70',
     className
   )
 
